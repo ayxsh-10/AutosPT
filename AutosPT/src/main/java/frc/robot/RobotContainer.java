037e0423530972248f09e5d4e33bda325aee409f
ayush <ayushnaruto35@gmail.com>
2025-09-13 11:49:19 -0700

    bro idk

--- AutosPT/src/main/java/frc/robot/RobotContainer.java
+++ AutosPT/src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,8 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.autos.TopThreePieceMid;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -58,6 +58,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return TopThreePieceMid.exampleAuto(m_exampleSubsystem);
   }
 }
