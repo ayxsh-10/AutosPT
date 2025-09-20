4b81c96cbb2faef1263f33cc96d0f08aa3c3cff6
ayush <ayushnaruto35@gmail.com>
2025-09-20 11:18:54 -0700

    autos 9/20
    helped by Anthony Ge

--- AutosPT/src/main/java/frc/robot/commands/autos/TopThreePieceMid.java
+++ AutosPT/src/main/java/frc/robot/commands/autos/TopThreePieceMid.java
@@ -6,17 +6,44 @@ package frc.robot.commands.autos;
 
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
 
 
-public final class TopThreePieceMid {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+public final class TopThreePieceMid extends SequentialCommandGroup {
+  private List<PathPlannerPath> pathlist;
+  public Pose2d startingPose;
+
+  public TopThreePieceMid(String autoname) throws IOException, org.json.simple.parser.ParseException {
+    pathlist = PathPlannerAuto.getPathGroupFromAutoFile(autoname);
+    startingPose = pathlist.get(0).getStartingDifferentialPose();
+
+    addCommands(
+      Commands.sequence(
+        AutoBuilder.followPath(pathlist.get(0)),
+        AutoBuilder.followPath(pathlist.get(1)),
+        AutoBuilder.followPath(pathlist.get(2))
+      )
+      
+    );
   }
 
-  private TopThreePieceMid() {
-    throw new UnsupportedOperationException("This is a utility class!");
+  public static Command exampleAuto(ExampleSubsystem m_exampleSubsystem) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'exampleAuto'");
   }
+  
+  
 }
